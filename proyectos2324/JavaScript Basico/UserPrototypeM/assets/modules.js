f44d16d79b50c2ad525b6e856508a88d52506b3a
PckProgramador <958patrick@gmail.com>
2023-10-19 14:39:38 +0200

    ejercicio copia de seguridad objetos literales

--- proyectos2324/JavaScript Basico/UserPrototypeM/assets/modules.js
+++ proyectos2324/JavaScript Basico/UserPrototypeM/assets/modules.js
@@ -46,11 +46,104 @@ export const usuarioLiteral = {
     return this._dni+" "+this._edad+" "+this._nombre;
     },
 };
+
+
+
+
+//NEW
 //uso de herencia por prototipos con FUNCIONES CONSTRUCTORES
+export function UsuarioFuncional(nombre,edad=0){
+    this._nombre=nombre;
+    this._edad=edad;
+    this._dni="";
+
+    //Getter y setter para el nombre
+
+    Object.defineProperty(this,"nombre",{
+        get:function(){
+            return this._nombre;
+        },
+        set:function(nuevoNombre){
+            this._nombre=nuevoNombre;
+        },
+    });
+
+    Object.defineProperty(this,"edad",{
+        get:function(){
+            return this._edad;
+        },
+        set:function(nuevaEdad){
+            this._edad=nuevaEdad;
+        },
+    });
+    
+    Object.defineProperty(this,"dni",{
+        get:function(){
+            return this._dni;
+        },
+        set:function(nuevoDni){
+            const letrasValidas="TRWAGMYFPDXBNJZSQVHLCKE";
+            const numeros=nuevoDni.substring(0,8);
+            const letra=nuevoDni.charAt(8);
+        
+            if(letrasValidas.charAt(numeros%23)===letra.toUpperCase()){
+                this._dni=nuevoDni;
+            }
+            return false;
+        },
+    });
+    
+};
+
+UsuarioFuncional.prototype.mostrarInfo= function () {
+    console.log(`Bienvenido ${this.nombre} , tu edad es ${this.edad})`);
+
+};
+
+UsuarioFuncional.saludo = function(){
+    return "Hola mundo";
+};
 
 
 
 
 
+//NEW
 //uso de herencia por prototipos con CLASES
+export class UsuarioClases {
+    constructor(nombre = " ", edad =0){
+        this._nombre=nombre;
+        this._edad=edad;
+        this._dni="";
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    get edad(){
+        return this._edad;
+    }
+    get dni(){
+        return this._dni;
+    }
+    set nombre(nuevoNombre){
+        this._nombre=nuevoNombre;
+    }
+    set edad(nuevaEdad){
+        this._edad=nuevaEdad;
+    }
+    set dni(nuevoDni){
+        this.validarDni(nuevoDni)?this._dni=nuevoDni:alert("dni invalido");
+    }
 
+    validarDni(dni){
+        const letrasValidas="TRWAGMYFPDXBNJZSQVHLCKE";
+        const numeros=dni.substring(0,8);
+        const letra=dni.charAt(8);
+        
+        if(letrasValidas.charAt(numeros%23)===letra.toUpperCase()){
+            return true;
+        }
+        return false;
+    }
+
+};
--- proyectos2324/JavaScript Basico/UserPrototypeM/script.js
+++ proyectos2324/JavaScript Basico/UserPrototypeM/script.js
@@ -1,60 +1,85 @@
 /**
  * Crear una aplicacion web que atraves de un formulario me permita recoger los datos nombre edad y dni 
- * cuando pulsemos el boton de insertar usuario automaticamente crearra utilizando la herencia 
- * por prototipos un usuario de tipo usuarioLiteral porque vamos a usar objetos LITERALEs y guardará en un array
- * dichos objetos almacenandolos en el localStorage con clave datosUsuarios
+ * cuando pulsemos el boton de insertar usuario automaticamente y validemos que la letra del dni es correcta,
+ *  
+ * 
+ * crearra utilizando la herencia por prototipos un usuario de tipo usuarioLiteral porque vamos a usar objetos LITERALES,funciones constructoras y clases 1 para cada uno, 
+ * 
+ * 
+ * y guardará en un arraydichos objetos almacenandolos en el localStorage con clave datosUsuarios, solo lo hará si el dni no está repetido.
+ * 
+ * 
  */
 
-
-
 // imports
-import { usuarioLiteral as Usuario, usuarioLiteral } from "./assets/modules.js";
-
-
-
+import { usuarioLiteral as Usuario } from "./assets/modules.js";
 ///variables globales
+let arrayPersonas= new Array();
 
-
-
-
-//funciones
-// let copiaSeguridad;
-// function handlerCopiarSeguridad(){
-//     copiaSeguridad=JSON.parse(localStorage.getItem("datosUsuarios"));
-//     return copiaSeguridad;
-// }
-// document.getElementById("insertarCopia").addEventListener("click",handlerCopiarSeguridad);
-
-
-let arrayPersonas=[];
-function handlerInsertarUsuario(event){  
-    arrayPersonas=JSON.parse(localStorage.getItem("datosUsuarios"));
+function handlerInsertarUsuario(event){ 
     event.preventDefault();
-
+    arrayPersonas=JSON.parse(localStorage.getItem("datosUsuarios"))?JSON.parse(localStorage.getItem("datosUsuarios")):[];
     //buena praxis traerse el formulario por si lo necesitases
     //const formularioUsuarios=document.getElementById("formulariousuarios").value;
     const usernameFrml=document.getElementById("username").value;
     const edadFrml=document.getElementById("edad").value;
     const dniFrml=document.getElementById("dni").value;
-
     //para instanciar o crear un objeto cuyo prototipo sea usuarioLiteral?
     //const objetoHijo=Object.create(usuarioLiteral);
-
     let newUser=Object.create(Usuario);
     newUser.nombre=usernameFrml;
     newUser.edad=edadFrml;
     if(newUser.dni=dniFrml){
-        console.log("traza");
-        console.log(newUser.mostrarInformacion());
+        if(dniDuplicado(dniFrml)){
+            mensajeExito.style.display="block";
+            mensajeError.style.display="none"; 
+            arrayPersonas.push(newUser);
+            localStorage.setItem("datosUsuarios",JSON.stringify(arrayPersonas));
+        }else{
+            mensajeExito.style.display="none";
+            mensajeError.style.display="block";
+        }   
     }else{
-        alert("no ha añadido el dni");
-    }
-    
-    arrayPersonas.push(newUser);
-    localStorage.setItem("datosUsuarios",JSON.stringify(arrayPersonas));
+        mensajeExito.style.display="none";
+        mensajeError.style.display="block";  
+    }   
 }
+
+
+
+
+function dniDuplicado(dni){
+    let validador=true;
+    if(JSON.parse(localStorage.getItem("datosUsuarios"))){
+        for(let objeto of JSON.parse(localStorage.getItem("datosUsuarios"))){
+            if(objeto._dni==dni){
+                validador=false
+            }
+        }
+    }
+    return validador;
+}
+
+function handlerCopiaSeguridad (event){
+    event.preventDefault();
+    let arrayCopiaSeguridad=new Array();
+    arrayCopiaSeguridad=JSON.parse(localStorage.getItem("datosUsuarios"))?JSON.parse(localStorage.getItem("datosUsuarios")):[];
+    let arrayPropiedades=sacarPropiedades(arrayCopiaSeguridad);
+    document.getElementById('usuariosTextArea').innerHTML = arrayPropiedades;
+
+}
+function sacarPropiedades(arrayObjetos){
+    let arrayPropiedades=[];
+    
+    for(let objeto of arrayObjetos){
+        arrayPropiedades.push(objeto._nombre+" edad:"+objeto._edad+" dni:"+objeto._dni );
+    }
+    return arrayPropiedades;
+}
+
 function init(){
     document.getElementById("insertarUsuario").addEventListener("click",handlerInsertarUsuario);
+    document.getElementById("insertarCopia").addEventListener("click",handlerCopiaSeguridad);
 }
 
 //inicio de la aplicacion
